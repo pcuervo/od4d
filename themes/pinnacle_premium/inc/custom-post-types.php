b261de4fb6910d39d8b82c3ef2508d2765d7b36b
alejandro <alex@losmaquiladores.com>
2016-04-01 10:44:40 -0600

    nuevo postype proyectos qeu alimenta la taxonomy proyecto

--- themes/pinnacle_premium/inc/custom-post-types.php
+++ themes/pinnacle_premium/inc/custom-post-types.php
@@ -19,9 +19,13 @@ add_action('init', function(){
 	insert_sector_taxonomy_terms();
 	$results_pt->add_taxonomy( 'Publication Type' );	
 	insert_publication_type_taxonomy_terms();
+	$results_pt->add_taxonomy( 'Proyecto' );	
+
 
 	// IMPLEMENTING PARTNERS
 	$implementin_partners_pt = register_cuztom_post_type( 'Implementing Partner' );
+
+	$implementin_partners_pt = register_cuztom_post_type( 'Proyecto' );
 
 });
 
@@ -91,7 +95,25 @@ function insert_publication_type_taxonomy_terms(){
 }// insert_publication_type_taxonomy_terms
 
 
+function create_terms_proyecto( $post_id, $post ) {
 
+	if ($post->post_type != 'proyecto' )
+		return '';
+
+	if ($post->post_status == 'trash') {
+		$category = get_term_by('slug', $post->post_name, 'proyecto');
+		wp_delete_term( $category->term_id, 'proyecto' );
+		return '';
+	}
+
+	$term_name = trim($post->post_title);
+
+	if ( ! term_exists( $post->post_name, 'proyecto' ) ){
+		wp_insert_term( $term_name, 'proyecto', array('slug' => $post->post_name) );
+	}
+
+}
+add_action( 'wp_insert_post', 'create_terms_proyecto', 10, 3 );
 
 
 
