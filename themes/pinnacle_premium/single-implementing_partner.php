89258d425e21b987831677c7d0f723ed6cd9f851
Raúl De Zamacona <raulppcuervo.com>
2016-04-04 16:05:33 -0500

    singlr implementing partner - if twitter, if url

--- themes/pinnacle_premium/single-implementing_partner.php
+++ themes/pinnacle_premium/single-implementing_partner.php
@@ -5,6 +5,7 @@
 	$website_link = get_implementing_partner_meta( $post->ID, '_official_website_meta' );
 	$twitter_username = get_implementing_partner_meta( $post->ID, '_twitter_username_meta' );
 	$rss_link = get_implementing_partner_meta( $post->ID, '_rss_link_meta' );
+	$widget_id = get_post_meta($post->ID, '_widget_id_meta', true);
 ?>
 
 	<div id="pageheader" class="titleclass">
@@ -31,23 +32,25 @@
 				<?php get_template_part('templates/implementing-partner', 'projects'); ?>
 			</div>
 			<aside class="[ tcol-ss-12 tcol-md-4 ]">
-				<div class="[ margin-bottom ]">
-					<h4 class="[ hometitle ]">Official website</h4>
-					<p class="[ text-center ]"><a href="<?php echo $website_link; ?>"><?php echo $website_link; ?></a></p>
-				</div>
-				<div class="[ margin-bottom ]">
-					<h4 class="[ hometitle ]">Twitter username</h4>
-					<a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank">@<?php echo $twitter_username; ?></a>
+				<?php if ( $website_link != '' ){ ?>
+					<div class="[ margin-bottom ]">
+						<h4 class="[ hometitle ]">Official website</h4>
+						<p class="[ text-center ]"><a href="<?php echo $website_link; ?>"><?php echo $website_link; ?></a></p>
+					</div>
+				<?php } ?>
+				<?php if ( $twitter_username != '' AND $widget_id != ''){ ?>
+					<div class="[ margin-bottom ]">
+						<h4 class="[ hometitle ]">Twitter feed</h4>
+						<a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank">@<?php echo $twitter_username; ?></a>
 
-					<div id="post-85" class="blog_item postclass" itemscope="" itemtype="http://schema.org/BlogPosting">
-						<?php $widget_id = get_post_meta($post->ID, '_widget_id_meta', true);
-						if ($widget_id != ''): ?>
-							<a class="twitter-timeline" href="https://twitter.com/<?php echo $twitter_username; ?>" data-widget-id="716012088699686912">Tweets por el @<?php echo $twitter_username; ?>.</a>
-							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-						<?php endif; ?>
-					</div> <!-- Blog Item -->
-
-				</div>
+						<div id="post-85" class="blog_item postclass" itemscope="" itemtype="http://schema.org/BlogPosting">
+							<?php if ($widget_id != ''): ?>
+								<a class="twitter-timeline" href="https://twitter.com/<?php echo $twitter_username; ?>" data-widget-id="716012088699686912">Tweets por el @<?php echo $twitter_username; ?>.</a>
+								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+							<?php endif; ?>
+						</div> <!-- Blog Item -->
+					</div>
+				<?php } ?>
 				<div class="[ margin-bottom ]">
 					<h4 class="[ hometitle ]">Accountability</h4>
 				</div>
