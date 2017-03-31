fbae994c73186ec6040691ea910ee3d0f34b6992
Arisbeth <aris@pcuervo.com>
2017-03-30 20:03:01 -0600

    robots.txt, y metas head

--- themes/pinnacle_premium/template-initiative.php
+++ themes/pinnacle_premium/template-initiative.php
@@ -8,20 +8,18 @@ Template Name: Initiative
 	
     <div id="content" class="container">
    	<div class="row">
-    <div class="main <?php echo kadence_main_class(); ?>" role="main">
-    	<div class="postclass pageclass clearfix entry-content" itemprop="mainContentOfPage">
+		<div class="main <?php echo kadence_main_class(); ?>" role="main">
+			<div class="postclass pageclass clearfix entry-content" itemprop="mainContentOfPage">
 
+				<div class="[ tcol-ss-12 tcol-lg-offset-2 tcol-lg-8  ]">
+					<?php the_post_thumbnail( 'full', array( 'class' => '[ margin-bottom ][ image-responsive image-centered ][ padding ]' ) ); ?>
+					<div class="[ content-initiative ][ margin-bottom ]">
+						<?php the_content(); ?>
+					</div>
+					<?php get_template_part('templates/related-results'); ?>
+				</div>				
 
-			<div class="[ tcol-ss-12 tcol-lg-offset-2 tcol-lg-8  ]">
-				<?php the_post_thumbnail( 'full', array( 'class' => '[ margin-bottom ][ image-responsive image-centered ][ padding ]' ) ); ?>
-				<div class="[ content-initiative ][ margin-bottom ]">
-					<?php the_content(); ?>
-				</div>
-				<?php get_template_part('templates/related-results'); ?>
-			</div>				
-
-			
-		</div>
-
-</div><!-- /.main -->
+				
+			</div>
+		</div><!-- /.main -->
   <?php get_footer(); ?>
--- themes/pinnacle_premium/templates/head.php
+++ themes/pinnacle_premium/templates/head.php
@@ -4,8 +4,10 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?> <?php kadence_html_tag_schema();?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> <?php kadence_html_tag_schema();?>> <!--<![endif]-->
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="OD4D is the leading global partnership to advance the creation of locally-driven and sustainable open data ecosystems around the world." >
+	<meta name="keywords" content="open data for development, open data, data, od4d, open data charter, open data network, idrc, crdi, open data research, open data roadmap, international development, open data ecosystems">
   <?php wp_head(); ?>
 </head>
